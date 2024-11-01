2f99ff57f6b3f06d8f9be124d9ca1262fec66c14
Aska Takahashi <aska.jpg@gmail.com>
2024-11-01 09:26:21 +0400

    home page style

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Header from "@/components/Header";
 
 export default function Home() {
@@ -5,8 +6,19 @@ export default function Home() {
     <>
       <Header />
       <main>
-        <h1 className="font-logo font-bold">DivLog</h1>
-        <p>base</p>
+        <div className="w-64 mx-auto my-24 text-center hover:cursor-pointer hover:text-darkBlueLight duration-150">
+          <p>The last dive was on</p>
+          <p className="text-4xl py-2">28 Sep 2024</p>
+          <p className="text-l">At Green Lagoon, Phillippines</p>
+        </div>
+        <div className="flex flex-col">
+          <Link href="#" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue rounded-md text-xl shadow-lg hover:bg-lightBlue duration-75">
+            Dive Plan
+          </Link>
+          <Link href="logBook" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue rounded-md text-xl shadow-lg hover:bg-lightBlue duration-75">
+            Log Book
+          </Link>
+        </div>
       </main>
     </>
   );
