707b1bea361ba0c2c505a83ea42756d998f1eba4
Aska Takahashi <aska.jpg@gmail.com>
2024-11-01 15:31:29 +0400

    modify: added link to add new log button

--- frontend/components/log/AddNewLogBtn.tsx
+++ frontend/components/log/AddNewLogBtn.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 const AddNewLogBtn = () => {
   return (
-    <button
-      className="w-12 h-12 rounded-full bg-eyeCatch text-darkBlue text-sm leading-3 text-center hover:bg-eyeCatchDark"
-    >
-      Add New
-    </button>
+    <Link href={'/logBook/add'}>
+      <button
+        className="w-12 h-12 rounded-full bg-eyeCatch text-darkBlue text-sm leading-3 text-center hover:bg-eyeCatchDark"
+      >
+        Add New
+      </button>
+    </Link>
   );
 }
 
