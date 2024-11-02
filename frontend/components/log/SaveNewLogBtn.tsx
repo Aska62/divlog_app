25431c6867ebf8f122ff318e138b1eb22ade6b4d
Aska Takahashi <aska.jpg@gmail.com>
2024-11-02 12:13:37 +0400

    Applied dark mode styles for add log page

--- frontend/components/log/SaveNewLogBtn.tsx
+++ frontend/components/log/SaveNewLogBtn.tsx
@@ -1,6 +1,6 @@
 const SaveNewLogBtn = () => {
   return (
-    <button className="bg-darkBlue hover:bg-darkBlueLight text-baseWhite px-3 rounded-md">Save</button>
+    <button className="bg-darkBlue dark:bg-iceBlue hover:bg-darkBlueLight dark:hover:bg-lightBlue text-baseWhite dark:text-baseBlack px-3 rounded-md">Save</button>
   );
 }
 
