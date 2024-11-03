dc23593caaf61ccda7fd688b1d7c1b6404b19930
Aska Takahashi <aska.jpg@gmail.com>
2024-11-03 15:50:15 +0400

    Added log edit page

---
+++ frontend/components/log/UpdateLogBtn.tsx
@@ -0,0 +1,7 @@
+const UpdateLogBtn = () => {
+  return (
+    <button className="bg-darkBlue dark:bg-iceBlue hover:bg-darkBlueLight dark:hover:bg-lightBlue text-baseWhite dark:text-baseBlack px-3 rounded-md">Save</button>
+  );
+}
+
+export default UpdateLogBtn;
