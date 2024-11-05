431d89cf301383534f1039c97c6176aa3d2f5386
Aska Takahashi <aska.jpg@gmail.com>
2024-11-05 08:34:22 +0400

    Modified docker settings to activate nextjs hotreload

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,5 +1,11 @@
 const LoginPage = () => {
-  return ( <>Login</> );
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <h1 className="font-logo text-darkBlue dark:text-lightBlue text-5xl">
+        DivLog
+      </h1>
+    </div>
+  );
 }
 
 export default LoginPage;
