f490b95f5eb254a0aabc2b5ed31c5422daddf766
Aska Takahashi <aska.jpg@gmail.com>
2024-11-08 12:00:18 +0400

    added user name input to register form

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -4,11 +4,25 @@ import RegisterBtn from "@/components/RegisterBtn";
 
 const RegisterPage = () => {
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center">
+    <main className="w-full h-fit flex flex-col items-center justify-center mt-16">
       <LogoLg />
 
       <form action="#" className="w-64 text-center mt-10">
         <h2>Register</h2>
+
+        {/* divlog name */}
+        <div className="h-14 mt-2 mb-10 text-start">
+          <input
+            type="text"
+            name="divlogName"
+            placeholder="username"
+            required
+            className="w-full h-9 px-2 rounded-md bg-lightBlue dark:bg-baseWhite text-black focus:outline-none"
+          />
+          <p className="text-eyeCatchDark dark:text-eyeCatch text-sm">Please input username</p>
+        </div>
+
+        {/* email */}
         <div className="h-14 mt-2 mb-10 text-start">
           <input
             type="email"
@@ -19,6 +33,8 @@ const RegisterPage = () => {
           />
           <p className="text-eyeCatchDark dark:text-eyeCatch text-sm">Please provide a valid email address</p>
         </div>
+
+        {/* password */}
         <div className="h-14 mb-10 text-left">
           <input
             type="password"
@@ -29,6 +45,8 @@ const RegisterPage = () => {
           />
           {/* <p className="text-eyeCatchDark dark:text-eyeCatch text-sm">Please input password</p> */}
         </div>
+
+        {/* password confirmation */}
         <div className="h-14 mb-10 text-left">
           <input
             type="confirmPassword"
