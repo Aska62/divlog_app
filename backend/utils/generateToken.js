ef11c9f8b9c1a40ab4d51ff8b61d1694ce77f7e5
Aska Takahashi <aska.jpg@gmail.com>
2024-11-28 10:34:12 +0400

    adjusted jwt expiry

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {expiresIn: '3d'});
+  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {expiresIn: '30d'});
 
   res.cookie('jwt', token, {
     httpOnly: true,
