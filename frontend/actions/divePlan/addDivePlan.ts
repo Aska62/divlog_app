d05899d12298de2c989c971d5d53870b14083514
Aska Takahashi <aska.jpg@gmail.com>
2025-02-03 14:47:57 +0400

    Add dive record frontend func

--- frontend/actions/divePlan/addDivePlan.ts
+++ frontend/actions/divePlan/addDivePlan.ts
@@ -1,31 +1,21 @@
 import axios, { isAxiosError } from "axios";
 import combineDateTime from "@/utils/dateTime/combineDateTime";
 import isNumString from "@/utils/isNumString";
-import { DiveRecordStateType } from '@/actions/diveRecord/updateDiveRecord';
+import { DivePlanStateType } from "./updateDivePlan";
 
-async function addDiveRecord(_previousState: DiveRecordStateType, formData: FormData):Promise<DiveRecordStateType> {
+async function addDivePlan(_previousState: DivePlanStateType, formData: FormData):Promise<DivePlanStateType> {
   const date = new Date(String(formData.get('date'))) || null;
   const starTime = formData.get('start_time') || null;
   const startDateTime =  starTime ? combineDateTime(new Date(date), String(starTime)) : null;
-  const endTime = formData.get('end_time') || null;
-  const endDateTime =  endTime ? combineDateTime(new Date(date), String(endTime)) : null;
 
   const data = {
-    log_no             : isNumString(formData.get('log_no')) ? formData.get('log_no') : null,
     date               : date,
     location           : formData.get('location'),
     country_id         : isNumString(formData.get('country_id')) ? formData.get('country_id') : null,
     purpose_id         : isNumString(formData.get('purpose_id')) ? formData.get('purpose_id') : null,
     course             : formData.get('course'),
-    weather            : formData.get('weather'),
-    surface_temperature: isNumString(formData.get('surface_temperature')) ? formData.get('surface_temperature') : null,
-    water_temperature  : isNumString(formData.get('water_temperature')) ? formData.get('water_temperature') : null,
     max_depth          : isNumString(formData.get('max_depth')) ? formData.get('max_depth') : null,
-    visibility         : isNumString(formData.get('visibility')) ? formData.get('visibility') : null,
     start_time         : startDateTime,
-    end_time           : endDateTime,
-    tankpressure_start : isNumString(formData.get('tankpressure_start')) ? formData.get('tankpressure_start') : null,
-    tankpressure_end   : isNumString(formData.get('tankpressure_end')) ? formData.get('tankpressure_end') : null,
     added_weight       : isNumString(formData.get('added_weight')) ? formData.get('added_weight') : null,
     suit               : formData.get('suit'),
     gears              : formData.get('gears'),
@@ -36,18 +26,17 @@ async function addDiveRecord(_previousState: DiveRecordStateType, formData: Form
     dive_center_str    : formData.get('dive_center_str'),
     dive_center_id     : formData.get('dive_center_id'),
     notes              : formData.get('notes'),
-    is_draft           : !!formData.get('is_draft'),
   }
 
   try {
-    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/diveRecords`,
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/divePlans`,
       { ...data },
       { withCredentials: true }
     );
 
     return {
       success: true,
-      message: 'Successfully added new record',
+      message: 'Successfully added new Plan',
       diveRecordId: res.data.id
     }
 
@@ -61,10 +50,10 @@ async function addDiveRecord(_previousState: DiveRecordStateType, formData: Form
     } else {
       return {
         success: false,
-        message: 'Record addition failed',
+        message: 'Plan addition failed',
       }
     }
   }
 }
 
-export default addDiveRecord;
+export default addDivePlan;
---
+++ frontend/app/plans/add/page.tsx
@@ -0,0 +1,9 @@
+const AddDivePlanPage = () => {
+  return (
+    <>
+      Add new plan page
+    </>
+  );
+}
+
+export default AddDivePlanPage;
