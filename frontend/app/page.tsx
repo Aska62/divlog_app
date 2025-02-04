bc322a75fde3a809c88ca6dbfd594082da330bd0
Aska Takahashi <aska.jpg@gmail.com>
2025-02-04 17:37:11 +0400

    modified top page links

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
       <main className="mt-20 md:mt-52 lg:mt-64">
         <WelcomeMessage />
         <div className="flex flex-col lg:flex-row max-w-xl mx-auto">
-          <Link href="#" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue dark:border-lightBlue rounded-md text-xl shadow-lg hover:bg-lightBlue dark:hover:bg-baseBlackLight duration-75">
+          <Link href="/plans" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue dark:border-lightBlue rounded-md text-xl shadow-lg hover:bg-lightBlue dark:hover:bg-baseBlackLight duration-75">
             Dive Plan
           </Link>
-          <Link href="logBook" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue dark:border-lightBlue rounded-md text-xl shadow-lg hover:bg-lightBlue dark:hover:bg-baseBlackLight duration-75">
+          <Link href="/logBook" className="mx-auto my-4 w-56 h-28 flex items-center justify-center border border-darkBlue dark:border-lightBlue rounded-md text-xl shadow-lg hover:bg-lightBlue dark:hover:bg-baseBlackLight duration-75">
             Log Book
           </Link>
         </div>
